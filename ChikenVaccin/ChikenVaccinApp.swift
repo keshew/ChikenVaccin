6ebb453625d983e5138c16fccfacb81c714c961c
keshew <artyom.korotckov2017@yandex.ru>
2025-10-19 13:04:46 +0500

    Initial Commit

---
+++ ChikenVaccin/ChikenVaccinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChikenVaccinApp.swift
+//  ChikenVaccin
+//
+//  Created by Артём Коротков on 19.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChikenVaccinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
